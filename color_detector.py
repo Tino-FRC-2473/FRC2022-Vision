828806b0bfbe20695239e54a4327a2834061ad1d
Akshat Mehta <33008157+ukshat@users.noreply.github.com>
2022-01-13 17:14:17 -0800

    Update color_detector.py

--- color_detector.py
+++ color_detector.py
@@ -1,21 +1,23 @@
+import cv2
+import numpy as np
+
 class ColorDetector:
     
-    def __init__(self, video_name, lower_bound, upper_bound):
-        # self.image = cv2.imread(image_name)
-        self.video = VideoFileGeneratorFile.VideoFileGenerator(video_name)
+    def __init__(self, image_name, lower_bound, upper_bound):
+        self.image = cv2.imread(image_name)
         self.lower_bound = np.array(lower_bound)
         self.upper_bound = np.array(upper_bound)
             
     def detect(self):
-        
-        image = self.video.get_next_frame()
 
         # Convert BGR to HSV
-        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
         
         mask = cv2.inRange(hsv, self.lower_bound, self.upper_bound)
-        output = cv2.bitwise_and(image,image, mask= mask)
+        output = cv2.bitwise_and(self.image,self.image, mask= mask)
         
-        cv2.imshow("Color Detected", np.hstack((image,output)))
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        return output
+        
+#         cv2.imshow("Color Detected", np.hstack((self.image, output)))
+#         cv2.waitKey(0)
+#         cv2.destroyAllWindows()
