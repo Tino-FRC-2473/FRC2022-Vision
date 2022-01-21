ded4035d1442fcea4ece1e93810e408f003bfa9e
ukshat <akshat@punatar.org>
2022-01-21 12:44:28 -0800

    Fixed style issues

--- color_detector.py
+++ color_detector.py
@@ -17,12 +17,12 @@ class ColorDetector:
         self.image = cv2.imread(self.image_name)
         self.lower_bound = np.array(lower_bound)
         self.upper_bound = np.array(upper_bound)
-            
+
     def detect(self):
 
         # Convert BGR to HSV
         hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
-        
+
         mask = cv2.inRange(hsv, self.lower_bound, self.upper_bound)
         output = cv2.bitwise_and(self.image, self.image, mask=mask)
 
