94f93f3aa31ac85f13c784f9a1c1dabd89212208
ukshat <akshat@punatar.org>
2022-01-28 17:51:43 -0800

    Updated detect() parameters

--- run.py
+++ run.py
@@ -14,18 +14,18 @@ def side_by_side_image(title, *images):
     show_image(title, np.concatenate(images))
 
 
-def detect(path, img, color):
-    original = cv2.imread(path + img)
+def detect(file, color):
+    original = cv2.imread(file)
     cd = ColorDetector(original)
     result = cd.detect(color)
     output = cv2.bitwise_and(original, original, mask=result)
-    side_by_side_image(f"Detecting {color} in {img}", original, output)
+    side_by_side_image(f"Detecting {color} in {file[52:]}", original, output)
 
 
 if __name__ == '__main__':
     path = "/Users/akshatmehta/Downloads/test_images_revision_2/"
     for img in listdir(path):
-        detect(path, img, "red")
+        detect(path + img, "red")
     for img in listdir(path):
-        detect(path, img, "blue")
-    # detect(path, "light_red_distance_50_angle_30_background_elements_N.jpg", "red")
+        detect(path + img, "blue")
+    # detect(path + "light_red_distance_50_angle_30_background_elements_N.jpg", "red")
