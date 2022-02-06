bfa457dfacee3316195d4663a8fd1c53751f16e0
ukshat <akshat@punatar.org>
2022-02-05 17:08:13 -0800

    Added detect_and_save() method, ran it on the 55 images

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ import cv2
 from color_detector import ColorDetector
 import numpy as np
 
+blank = cv2.imread("blank.jpg")
+
 
 def show_image(title, image):
     cv2.imshow(title, image)
@@ -10,22 +12,25 @@ def show_image(title, image):
     cv2.destroyAllWindows()
 
 
-def side_by_side_image(title, *images):
-    show_image(title, np.concatenate(images))
+def detect_and_show(file, color):
+    image = cv2.imread(file)
+    cd = ColorDetector(image)
+    blurred, hsv, mask = cd.detect(color, True)
+    output = cv2.bitwise_and(blank, blank, mask=mask)
+    show_image(f"Detecting {color} in {file[52:]}", np.vstack((np.hstack((image, blurred)), np.hstack((hsv, output)))))
 
 
-def detect(file, color):
+def detect_and_save(file, color):
     original = cv2.imread(file)
     cd = ColorDetector(original)
     result = cd.detect(color)
-    output = cv2.bitwise_and(original, original, mask=result)
-    side_by_side_image(f"Detecting {color} in {file[52:]}", original, output)
+    cv2.imwrite(f"/Users/akshatmehta/Downloads/binary/{color}_{file[52:]}", result)
 
 
 if __name__ == '__main__':
     path = "/Users/akshatmehta/Downloads/test_images_revision_2/"
     for img in listdir(path):
-        detect(path + img, "red")
+        detect_and_show(path + img, "red")
     for img in listdir(path):
-        detect(path + img, "blue")
-    # detect(path + "light_red_distance_50_angle_30_background_elements_N.jpg", "red")
+        detect_and_show(path + img, "blue")
+    # detect_and_show(path + "light_red_distance_50_angle_30_background_elements_N.jpg", "red")
