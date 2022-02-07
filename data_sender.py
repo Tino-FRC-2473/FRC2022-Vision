99074337798aa5c161acbf7a869f2fefb03eb2dc
arjunsudheer <arjunsudheer10@gmail.com>
2022-02-06 16:04:59 -0800

    Add log and error files to shell script.
    Add log message to data_sender.py.

--- data_sender.py
+++ data_sender.py
@@ -6,3 +6,4 @@ camera_stream = VideoLiveGenerator()
 for i in range(100):
     image = camera_stream.get_next_frame()
     cv2.imwrite(f"image_{i}.jpg", image)
+    print("Testing")
--- run_2022_vision.sh
+++ run_2022_vision.sh
@@ -1,18 +1,28 @@
 #!bin/bash
-# add this shebang to run this shell script by the bash shell
+# add the shebang above to run this shell script by the bash shell
+
+#create the cv log file to log code actions
+cvlogfile="cv_log_$(date +"%m-%d-%Y_%H-%M-%S").txt"
+#create the cv error file to log code errors
+cverrorfile="cv_error_$(date +"%m-%d-%Y_%H-%M-%S").txt"
+
+whoami > $cvlogfile
+
+date >> $cvlogfile
 
 # change directory to where data_sender.py is located
 cd /home/nvidia/FRC2022-Vision
 
-# if the user does not have executable permissions for data_sender.py, give it to them
+# if the user does not have executable permission for data_sender.py, add the permission
 if [[ ! -x "data_sender.py" ]]
     then
-        echo "user does not have permission to execute the file: data_sender.py, adding execute permission"
+        echo "user does not have permission to execute the file: data_sender.py, adding execute permission" >> $cvlogfile
         chmod +x "data_sender.py"
-        echo "execute permission added"
+        echo "execute permission added" >> $cvlogfile
 fi
 
-# run the data_sender.py file
-python3 data_sender.py
+echo "data_sender.py has been started" >> $cvlogfile
 
-echo "data_sender.py has been started"
+# run the data_sender.py file
+#append outputs (default, so '1' not specified) to "cvlogfile", and redirect errors ('2') to "cverrorfile"
+sudo python3 data_sender.py >> $cvlogfile 2> $cverrorfile
