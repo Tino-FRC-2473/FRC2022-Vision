aad0bb2f32225fcfc91a6afa3c8aa11e232fa740
ukshat <akshat@punatar.org>
2022-02-16 09:32:18 -0800

    Finalized YUV bounds

--- shadow_removal_2.py
+++ shadow_removal_2.py
@@ -1,20 +1,28 @@
 
 from cv2 import cv2
-from numpy import array
-
-img_blue = cv2.imread('dark_blue_distance_24_angle_-30_background_elements_N.jpg')
-img_red = cv2.imread('dark_red_distance_65_angle_-30_background_elements_N.jpg')
-
-YUV_img = cv2.cvtColor(img_blue, cv2.COLOR_RGB2YUV)
-
-BLUE_LOWER = array([0, 0, 100])
-BLUE_UPPER = array([200, 125, 200])
-
-blurred = cv2.blur(YUV_img, (15, 15))
-
-mask = cv2.inRange(blurred, BLUE_LOWER, BLUE_UPPER)
+import numpy as np
+from color_detector import ColorDetector
 
 
-cv2.imshow('YUV img', mask)
+img_blue = cv2.imread('/Users/akshatmehta/Downloads/test_images_revision_2/dark_blue_distance_65_angle_-45_background_elements_N.jpg')
+img_red = cv2.imread('/Users/akshatmehta/Downloads/test_images_revision_2/light_red_distance_40_angle_-60_background_elements_N.jpg.jpg')
+
+blurred = cv2.blur(img_red, (15, 15))
+
+YUV_img = cv2.cvtColor(blurred, cv2.COLOR_BGR2YUV)
+
+# BLUE_LOWER = np.array([0, 130, 0])
+# BLUE_UPPER = np.array([130, 200, 200])
+
+RED_LOWER = np.array([0, 50, 150])
+RED_UPPER = np.array([200, 150, 255])
+
+mask = cv2.inRange(YUV_img, RED_LOWER, RED_UPPER)
+
+blank = cv2.imread("blank.jpg")
+mask = cv2.bitwise_and(blank, blank, mask=mask)
+hsv = ColorDetector(img_red).detect("red")
+hsv = cv2.bitwise_and(blank, blank, mask=hsv)
+cv2.imshow('YUV img', np.hstack((np.vstack((img_red, YUV_img)), np.vstack((hsv, mask)))))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
