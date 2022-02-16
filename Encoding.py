7de2ed86e1a3ad46d47451ffa445003419c3e840
Datta Kansal <55328687+LegendaryDK@users.noreply.github.com>
2022-02-16 00:20:46 -0800

    Update Encoding.py

--- Encoding.py
+++ Encoding.py
@@ -20,6 +20,6 @@ class Encoding:
 
     def send_data(self, ball_data, s): # ball_data: array from cv, s: serial port opened by cv
         data_string = "".join("\n{d}\n{d}\n".format(round(ball_data[0], 2), round(ball_data[1], 2))) # ball_data[0] = distance, #ball_data[1] = angle
-        # one line break: new value (angle), two line breaks: end of data
+        # one line break: new value
         print("X " + data_string + " E")
         s.write(bytes("X" + data_string + " E", "utf-8")) # "X" = start, "E" = end
