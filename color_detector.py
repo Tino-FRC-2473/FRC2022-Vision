e2099c0811fb5b8c334fc42bcfc71bf55bf72e00
arjunsudheer <arjunsudheer10@gmail.com>
2022-02-17 17:30:54 -0800

    Remove class structure. Update data_sender.py
    Add timer and update color and ball detection calls.

--- color_detector.py
+++ color_detector.py
@@ -2,29 +2,24 @@ import cv2
 from numpy import array
 
 
-class ColorDetector:
+BLUE_LOWER = array([70, 50, 50])
+BLUE_UPPER = array([120, 255, 255])
 
-    BLUE_LOWER = array([70, 50, 50])
-    BLUE_UPPER = array([120, 255, 255])
+RED_LOWER = array([120, 50, 50])
+RED_UPPER = array([200, 255, 255])
 
-    RED_LOWER = array([120, 50, 50])
-    RED_UPPER = array([200, 255, 255])
 
-    def __init__(self, image):
-        self.image = image
+def detect(image, color):
+    if color == "blue":
+        lower_bound = BLUE_LOWER
+        upper_bound = BLUE_UPPER
+    elif color == "red":
+        lower_bound = RED_LOWER
+        upper_bound = RED_UPPER
+    else:
+        raise Exception('color must be "blue" or "red"')
 
-    def detect(self, color):
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower_bound, upper_bound)
 
-        if color == "blue":
-            lower_bound = self.BLUE_LOWER
-            upper_bound = self.BLUE_UPPER
-        elif color == "red":
-            lower_bound = self.RED_LOWER
-            upper_bound = self.RED_UPPER
-        else:
-            raise Exception('color must be "blue" or "red"')
-
-        hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(hsv, lower_bound, upper_bound)
-
-        return mask
+    return mask
--- data_sender.py
+++ data_sender.py
@@ -1,18 +1,34 @@
 # from multiprocessing import Process
+import time
+# import serial
 from video_live_generator import VideoLiveGenerator
-from color_detector import ColorDetector
+import color_detector
 from ball_detection import BallDetection
 
 
 driver_camera = VideoLiveGenerator(1)
+# track the time to close the camera once the match is over
+start_time = time.time()
 
-while True:
+# open the serial port
+# ser = serial.Serial('/dev/')
+# print(ser.name)
+
+# run the cv code for 150 seconds (15 for autonomous + 125 for TeleOp)
+while (time.time() - start_time) < 150:
     next_img = driver_camera.get_next_frame()
-    # create a new instance of the ColorDetector class and BallDetection class to get ball information
-    color_detection = ColorDetector(next_img)
-    binary_image = color_detection.detect()
-    ball_detector = BallDetection(binary_image)
+    # get the binary image
+    binary_image = color_detector.detect(next_img, "blue")
+    # get the ball distance and angle from the robot
+    ball_detector = BallDetection(next_img)
     ball_info = ball_detector.detect_ball()
+    # get the needed information and start encoding
+    cv_info = [float(ball_info[0]), float(ball_info[1]), next_img]
+    print(f"cv_info is: {cv_info}")
+
+# close the camera at the end of the time
+driver_camera.close_camera()
+
 
 # auto_camera = VideoLiveGenerator(2)
 
