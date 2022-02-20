903b7eb4e62ed087d7a9854772ea077e6ae68340
ukshat <akshat@punatar.org>
2022-02-20 11:46:13 -0800

    Calculated YUV bounds

---
+++ create_bounds.py
@@ -0,0 +1,25 @@
+import cv2
+from os import listdir
+import statistics
+
+path = "/Users/akshatmehta/Downloads/benchmark/"
+Y = []
+U = []
+V = []
+for file in listdir(path):
+    if "blue" not in file:
+        continue
+    test = cv2.imread(path + file)
+    correct = cv2.imread(
+        f"/Users/akshatmehta/Downloads/test_images_revision_2/{file[file.index('benchmark_') + len('benchmark_'):]}")
+    correct = cv2.cvtColor(correct, cv2.COLOR_BGR2YUV)
+
+    for i in range(0, len(test), 2):
+        for j in range(0, len(test[i]), 2):
+            if test[i][j][0] > 128:
+                Y.append(correct[i][j][0])
+                U.append(correct[i][j][1])
+                V.append(correct[i][j][2])
+print(f"Y: mean: {statistics.mean(Y)}, stdev: {statistics.stdev(Y)}")
+print(f"U: mean: {statistics.mean(U)}, stdev: {statistics.stdev(U)}")
+print(f"V: mean: {statistics.mean(V)}, stdev: {statistics.stdev(V)}")
--- measure_accuracy.py
+++ measure_accuracy.py
@@ -2,28 +2,46 @@ import cv2
 from os import listdir
 import numpy as np
 
-path = "/Users/akshatmehta/Downloads/benchmark/"
-for i in listdir(path):
+accuracies = []
+ball_accuracies = []
 
-    test = cv2.imread(path + i)
-    if "blue" in i[:30]:
+path = "/Users/akshatmehta/Downloads/benchmark/"
+for file in listdir(path):
+
+    test = cv2.imread(path + file)
+    if "blue" in file[:30]:
         color = "blue"
-    elif "red" in i[:30]:
+    elif "red" in file[:30]:
         color = "red"
     else:
         continue
-    correct = cv2.imread(f"/Users/akshatmehta/Downloads/binary/{color}_{i[i.index('benchmark_') + len('benchmark_'):]}")
+
+    correct = cv2.imread(f"/Users/akshatmehta/Downloads/binary/{color}_{file[file.index('benchmark_') + len('benchmark_'):]}")
 
     matches = 0
+    ball_matches = 0
+    ball_size = 0
+
+    print(f"{color} in {file[file.index('benchmark_') + len('benchmark_'):]}")
 
     for i in range(len(test)):
         for j in range(len(test[i])):
             if test[i][j][0] == correct[i][j][0]:
                 matches += 1
+            if test[i][j][0] > 128:
+                ball_size += 1
+                if correct[i][j][0] > 128:
+                    ball_matches += 1
 
-    accuracy = matches / (correct.size / 3)
-    print(f"Accuracy: {accuracy:%}")
-    # if accuracy < 0.95:
-    #     cv2.imshow(f"Accuracy: {accuracy:%}", np.vstack((test, correct)))
-    #     cv2.waitKey(0)
-    #     cv2.destroyAllWindows()
+    accuracies.append(matches / (correct.size / 3))
+    ball_accuracies.append(ball_matches / ball_size)
+    print(f"Total Accuracy: {accuracies[-1]:%}")
+    print(f"Ball Accuracy: {ball_accuracies[-1]:%}\n")
+    # cv2.imshow(f"Accuracy: {accuracies[-1]:%}, Ball Accuracy: {ball_accuracies[-1]:%}", np.vstack((test, correct)))
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+print(accuracies)
+print(ball_accuracies)
+print(sum(accuracies)/len(accuracies))
+print(sum(ball_accuracies)/len(ball_accuracies))
