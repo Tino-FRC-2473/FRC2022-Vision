d89636fddc542184fe2fa9fd35cde71779812fed
arjunsudheer <arjunsudheer10@gmail.com>
2022-02-20 23:45:14 -0800

    Make CV code run indefinitely.
    Remove timer restriction.

--- data_sender.py
+++ data_sender.py
@@ -1,4 +1,3 @@
-import time
 import serial
 import struct
 from video_live_generator import VideoLiveGenerator
@@ -7,16 +6,13 @@ from ball_detection import BallDetection
 
 
 auto_camera = VideoLiveGenerator(1)
-# track the time to close the camera once the match is over
-start_time = time.time()
 
 # open the serial port
 ser = serial.Serial('/dev/')
 print(ser.name)
 
-
-# run the cv code for 150 seconds (15 for autonomous + 125 for TeleOp)
-while (time.time() - start_time) < 150:
+# run ball detection indefinitely
+while True:
     next_img = auto_camera.get_next_frame()
     # get the binary image
     binary_image = color_detector.detect(next_img, "blue")
@@ -29,6 +25,3 @@ while (time.time() - start_time) < 150:
     # typecast to bytearray for compatability with serial write() method
     print(bytearray(struct.pack("ff", float(cv_info[0]), float(cv_info[1]))))
     ser.write(bytearray(struct.pack("ff", float(cv_info[0]), float(cv_info[1]))))
-
-# close the camera at the end of the time
-auto_camera.close_camera()
