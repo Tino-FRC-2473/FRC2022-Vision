c6c669e540a32ebd45ed0ad450cbf057f759d434
arjunsudheer <arjunsudheer10@gmail.com>
2022-02-20 18:31:48 -0800

    Add initial code to run on LimeLight.
    Bring existing color and ball detection code. Add image annotation code.

---
+++ limelight.py
@@ -0,0 +1,142 @@
+import cv2
+import numpy as np
+import imutils
+
+# Camera Constants
+KNOWN_DIAMETER_IN = 9.5  # in
+# ADJ_FOCAL_LENGTH = 1480.1
+FOCAL_LENGTH = 1367.043233  # 83.3% accuracy. Use 1645.101 -> 99.73% accuracy but overshoots
+RADIUS_THRESH = 85
+MIN_BORDERS = 6
+CAMERA_TILT_DOWNWARDS = 20
+
+
+# CV Input Code
+def set_up_camera(camera_port):
+    camera_path = "/dev/video" + str(camera_port)
+
+    # set the width and height property of the image
+    live_input.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    live_input.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+    # use subprocess to set camera settings
+    try:
+        subprocess.check_call(["v4l2-ctl", "-d", camera_path, "-c", "white_balance_temperature_auto=0"])
+        subprocess.check_call(["v4l2-ctl", "-d", camera_path, "-c", "white_balance_temperature=5400"])
+        subprocess.check_call(["v4l2-ctl", "-d", camera_path, "-c", "exposure_auto=1"])
+        subprocess.check_call(["v4l2-ctl", "-d", camera_path, "-c", "exposure_absolute=5"])
+        print(subprocess.check_output(["v4l2-ctl", "-d", camera_path, "-C", "exposure_absolute"]))
+        print("Camera setup complete")
+    except CalledProcessError as cpe:
+        print("Called Process Error occurred. Camera setup incomplete!")
+        print("Error status:" + str(cpe.returncode))
+    except FileNotFoundError:
+        print("v4l2 not found. Camera setup incomplete!")
+
+
+def draw_image_annotations(image, angle, distance):
+    # PLEASE MAKE SURE THAT THIS IMAGE IS PASSED BY REFERENCE (IF '=' IS USED, RETURN THE IMAGE INSTEAD)
+    cv2.putText(image, f"Angle: {angle} deg.", (875, 100), 0, 1.5, (0, 255, 0), 3)
+    cv2.putText(image, f"Distance: {distance} in.", (875, 200), 0, 1.5, (0, 255, 0), 3)
+    cv2.arrowedLine(image, (650, 700), (int(650 + distance * 10.77 * math.cos(math.radians(90 - angle))), int(700 - distance * 10.77 * math.sin(math.radians(90 - angle)))), (0, 255, 255), 2, 8, 0, 0.1)
+
+
+# CV Color Detection Code
+BLUE_LOWER = np.array([70, 50, 50])
+BLUE_UPPER = np.array([120, 255, 255])
+
+RED_LOWER = np.array([120, 50, 50])
+RED_UPPER = np.array([200, 255, 255])
+
+
+def detect(image, color):
+    if color == "blue":
+        lower_bound = BLUE_LOWER
+        upper_bound = BLUE_UPPER
+    elif color == "red":
+        lower_bound = RED_LOWER
+        upper_bound = RED_UPPER
+    else:
+        raise Exception('color must be "blue" or "red"')
+
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower_bound, upper_bound)
+
+    return mask
+
+
+# CV Ball Detection Code
+class BallDetection:
+
+    def __init__(self, frame):
+        self.frame = frame
+        self.x_val = int(frame.shape[1])
+        self.y_val = int(frame.shape[0])
+
+    def detect_ball(self):
+        distance = -1
+        angle = -1
+        frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+        self.prep_frame()
+        cnts = cv2.findContours(frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        cnts = imutils.grab_contours(cnts)
+
+        if len(cnts) > 0:
+            for cnt in cnts:
+                ((x, y), radius) = cv2.minEnclosingCircle(cnt)
+                if radius >= RADIUS_THRESH:
+                    # determine the num of border points on the contour (ex: differentiates between circle and rect)
+                    perimeter = cv2.arcLength(cnt, closed=True)
+                    borders = cv2.approxPolyDP(curve=cnt, epsilon=0.0085 * perimeter, closed=True)
+                    if len(borders) > MIN_BORDERS:
+                        angle = self.find_angle(x, y)
+                        # print(radius)
+                        distance = self.find_distance(radius)
+                    cv2.circle(self.frame, (int(x), int(y)), int(radius), (0, 255, 0), 2)
+        return distance, angle
+
+    def transform(self, x, y):
+        # transform coordinates to a system with the origin at the bottom-middle of screen
+        dims = self.frame.shape
+        x = x - (dims[1] / 2)
+        y = dims[0] - y
+        return x, y
+
+    def find_angle(self, x, y):
+        tsfm_x, tsfm_y = self.transform(x, y)
+        angle_rad = np.arctan(abs(tsfm_x) / tsfm_y)
+        angle_deg = round(np.degrees(angle_rad), 2)
+
+        # angle annotations
+        cv2.putText(self.frame, "Angle: " + str(angle_deg) + " degrees", (50, 30), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                    (200, 200, 0), 1)
+        cv2.line(self.frame, (int(self.x_val / 2), self.y_val), (int(x), int(y)), (0, 0, 255), 2)
+        return angle_deg
+
+    def find_distance(self, radius):
+        dist = (KNOWN_DIAMETER_IN * FOCAL_LENGTH) / (radius * 2)
+        # print("Distance: " + str(dist))
+        return dist
+
+    def prep_frame(self):
+        # draw axes on screen for visualization
+        cv2.line(self.frame, (int(self.x_val / 2), self.y_val), (int(self.x_val / 2), 0), (0, 0, 255), 2)
+        cv2.line(self.frame, (0, self.y_val), (self.x_val, self.y_val), (0, 0, 255), 3)
+
+
+# runPipeline() is called every frame by Limelight's backend.
+def runPipeline(image, llrobot):
+    set_up_camera(1)
+
+    binary_image = detect(image, "blue")
+
+    ball_detect = BallDetection(binary_image)
+    distance, angle = ball_detect.detect_ball()
+
+    draw_image_annotations(image, angle, distance)
+
+    # record the distance and angle of the ball to the robot to send back to the robot
+    llpython = [float(distance), float(angle)]
+
+    # return the modified image, and custom robot data
+    return image, llpython
