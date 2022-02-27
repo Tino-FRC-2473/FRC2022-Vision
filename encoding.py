13d653087f20d2fbdaa7bf7ae3dd1d8070724ace
LegendaryDK <55328687+LegendaryDK@users.noreply.github.com>
2022-02-26 21:45:50 -0800

    remove init function and .idea file

--- encoding.py
+++ encoding.py
@@ -3,17 +3,6 @@ import ball_detection as bd
 
 
 class Encoding:
-    def __init__(self):
-        pass
-        # code to test:
-        # def __init__(self, name = "ttyS0"):
-        # self.s = serial.Serial("/dev/")
-        # print(s.name)
-        # s.write('Hello World!')
-        # s.close()
-        # img = cv2.imread("test_images/blue_dark_blue_distance_24_angle_30_background_elements_N.jpg")
-        # ball = bd.BallDetection(img)
-
     def send_data(self, ball_data, s):   # ball_data: array from cv, s: serial port opened by cv
         data_string = "".join("\nD{:.2f}\nA{:.2f}\n".format(round(ball_data[0], 2), round(ball_data[1], 2)))
         # ball_data[0] = distance, #ball_data[1] = angle
