4e7bb20400cf11a4d73a7d693dd82a3272cfb886
ukshat <akshat@punatar.org>
2022-03-03 09:56:23 -0800

    HSV instead of YUV for blue

--- color_detector.py
+++ color_detector.py
@@ -5,18 +5,20 @@ import numpy as np
 RED_LOWER = np.array([0, 50, 140])
 RED_UPPER = np.array([200, 150, 255])
 
-BLUE_LOWER = np.array([0, 0, 0])
-BLUE_UPPER = np.array([255, 255, 110])
+BLUE_LOWER = np.array([70, 50, 50])
+BLUE_UPPER = np.array([120, 255, 255])
 
 
 def detect(image, color, return_all=False):
 
     blurred = cv2.blur(image, (15, 15))
-    yuv = cv2.cvtColor(blurred, cv2.COLOR_BGR2YUV)
 
     if color == "blue":
-        mask = cv2.inRange(yuv, BLUE_LOWER, BLUE_UPPER)
+        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, BLUE_LOWER, BLUE_UPPER)
+        yuv = hsv
     elif color == "red":
+        yuv = cv2.cvtColor(blurred, cv2.COLOR_BGR2YUV)
         mask = cv2.inRange(yuv, RED_LOWER, RED_UPPER)
     else:
         raise Exception('color must be "blue" or "red"')
